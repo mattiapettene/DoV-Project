a98235b86d3cfc1e898cf87b921f522efcf7a311
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2023-04-20 14:44:00 +0200

    Update Maple file using fy0 instead of fy in mz0

--- Assignements/Assignement-01-tyre_fitting/tyre_lib/MF96_MZ0.m
+++ Assignements/Assignement-01-tyre_fitting/tyre_lib/MF96_MZ0.m
@@ -5,11 +5,11 @@ function [mz0] = MF96_MZ0(kappa, alpha, phi, Fz, tyre_data)
 
   [Br, Bt, Ct, Dr, Dt, Et, alpha__r, alpha__t] = MF96_MZ0_coeffs(kappa, alpha, phi, Fz, tyre_data);
                  mzr = Dr*cos(arctan(Br*alpha__r))*cos(alpha);
-                 fy = MF96_FY(kappa,alpha,phi,Fz, tyre_data);
+                 fy0 = MF96_FY0(kappa,alpha,phi,Fz, tyre_data);
                  t = Dt*cos(Ct*arctan(Bt*alpha__t-Et(Bt*alpha__t-arctan(Bt*alpha__t))))*cos(alpha);
 
  % main code
 
-  mz0 = -t * fy + mzr;
+  mz0 = -t * fy0 + mzr;
   
  end
