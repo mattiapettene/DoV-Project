faf844f0789df424e114781051ed860d7802a1c1
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2023-04-29 09:24:28 +0200

    Updated Maple file with last changes and generated new functions

--- Assignements/Assignement-01-tyre_fitting/tyre_lib/MF96_MZ0.m
+++ Assignements/Assignement-01-tyre_fitting/tyre_lib/MF96_MZ0.m
@@ -6,7 +6,7 @@ function [mz0] = MF96_MZ0(kappa, alpha, phi, Fz, tyre_data)
   [Br, Bt, Ct, Dr, Dt, Et, alpha__r, alpha__t] = MF96_MZ0_coeffs(kappa, alpha, phi, Fz, tyre_data);
                  mzr = Dr*cos(arctan(Br*alpha__r))*cos(alpha);
                  fy0 = MF96_FY0(kappa,alpha,phi,Fz, tyre_data);
-                 t = Dt*cos(Ct*arctan(Bt*alpha__t-Et(Bt*alpha__t-arctan(Bt*alpha__t))))*cos(alpha);
+                 t = Dt*cos(Ct*atan(Bt*alpha__t-Et*(Bt*alpha__t-atan(Bt*alpha__t))))*cos(alpha);
 
  % main code
 
