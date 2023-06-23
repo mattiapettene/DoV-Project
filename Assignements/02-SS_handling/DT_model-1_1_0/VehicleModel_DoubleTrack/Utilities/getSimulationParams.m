5e11ade6b3c3b8d9d1e40547156faf56b0683147
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2023-06-23 16:02:38 +0200

    Update range for fitting cornering stiffnesses

--- Assignements/02-SS_handling/DT_model-1_1_0/VehicleModel_DoubleTrack/Utilities/getSimulationParams.m
+++ Assignements/02-SS_handling/DT_model-1_1_0/VehicleModel_DoubleTrack/Utilities/getSimulationParams.m
@@ -2,8 +2,8 @@ function simulation = getSimulationParams()
  
     % Timings
     times.t0        = 0;     % [s]  <--- starting time
-    times.step_size = 1e-4; % [s]  <--- discrete solver step
-    times.tf        = 150;    % [s]  <--- stop simulation time
+    times.step_size = 0.5e-4; % [s]  <--- discrete solver step
+    times.tf        = 100;    % [s]  <--- stop simulation time
     
     simulation.times = times;
 
