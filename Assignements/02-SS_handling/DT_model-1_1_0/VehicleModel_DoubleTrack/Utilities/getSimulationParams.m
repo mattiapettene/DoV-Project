85af637b8117aaf9b12a022fa21fe1001eb31582
NataleConsalvi <nataleconsalvi06@gmail.com>
2023-06-23 10:04:55 +0200

    Tf modified, also Ts (100s, 0.5e^-4)

--- Assignements/02-SS_handling/DT_model-1_1_0/VehicleModel_DoubleTrack/Utilities/getSimulationParams.m
+++ Assignements/02-SS_handling/DT_model-1_1_0/VehicleModel_DoubleTrack/Utilities/getSimulationParams.m
@@ -3,7 +3,7 @@ function simulation = getSimulationParams()
     % Timings
     times.t0        = 0;     % [s]  <--- starting time
     times.step_size = 0.5e-4; % [s]  <--- discrete solver step
-    times.tf        = 200;    % [s]  <--- stop simulation time
+    times.tf        = 100;    % [s]  <--- stop simulation time
     
     simulation.times = times;
 
